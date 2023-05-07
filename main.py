e05c8ea9e12fc3552d46b34f913aefac535cd803
nxmirich <nxmirich@gmail.com>
2023-05-07 16:40:56 +0300

    now it can walk left and right

--- main.py
+++ main.py
@@ -11,17 +11,19 @@ bg = pygame.transform.scale((pygame.image.load("bg.png")), (width, height))
 player_width = 40
 player_height = 60
 
+player_vel = 20
+
 
 def draw(player):
     win.blit(bg, (0, 0))
-    pygame.draw.rect(win, (255, 200, 200), player)
+    pygame.draw.rect(win, (255, 0, 0), player)
     pygame.display.update()
 
 
 def main():
     run = True
 
-    player = pygame.Rect(663, height - player_height, player_width, player_height)
+    player = pygame.Rect(663, 510, player_width, player_height)
 
     while run:
         for event in pygame.event.get():
@@ -29,7 +31,11 @@ def main():
                 run = False
                 break
         draw(player)
-
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.x -= player_vel
+        if keys[pygame.K_RIGHT]:
+            player.x += player_vel
     pygame.quit()
 
 
