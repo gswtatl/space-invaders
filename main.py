fcdcc0cb5eda6711a6eda05f51c7efa9223b683a
nxmirich <nxmirich@gmail.com>
2023-05-07 16:21:48 +0300

    added player!!

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ import random
 
 width, height = 1366, 768
 win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Happy Birthday!")
+pygame.display.set_caption("game")
 
-bg = pygame.image.load("bg.png")
+bg = pygame.transform.scale((pygame.image.load("bg.png")), (width, height))
 
-def draw():
-    win.blit(bg, (0,0))
+player_width = 40
+player_height = 60
+
+
+def draw(player):
+    win.blit(bg, (0, 0))
+    pygame.draw.rect(win, (255, 200, 200), player)
     pygame.display.update()
+
+
 def main():
     run = True
+
+    player = pygame.Rect(663, height - player_height, player_width, player_height)
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw()
+        draw(player)
 
     pygame.quit()
 
