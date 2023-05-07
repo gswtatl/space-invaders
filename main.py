e3fc5789eb5615d01932295d724c84ece9c66c85
nxmirich <nxmirich@gmail.com>
2023-05-07 16:05:57 +0300

    bg and .gitignore

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import time
 import random
 
 width, height = 1366, 768
-window = pygame.display.set_mode((width, height))
+win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Happy Birthday!")
 
+bg = pygame.image.load("bg.png")
 
+def draw():
+    win.blit(bg, (0,0))
+    pygame.display.update()
 def main():
     run = True
 
@@ -15,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw()
 
     pygame.quit()
 
